b05313a145a7585e49c9ccad8d276e3d9bc3c8a7
HoogaBoga <matty.lim718@gmail.com>
2025-05-24 20:11:41 +0800

    Not done with the register page but has progress

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        // Add any other paths where you use Tailwind classes
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
