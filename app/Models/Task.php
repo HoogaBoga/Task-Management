5da9aff33073c5c53d727782d861c96ac98cfe38
HoogaBoga <matty.lim718@gmail.com>
2025-06-12 04:42:37 +0800

    ??

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -161,33 +161,4 @@ class Task extends Model
             $this->attributes['category'] = json_encode(is_array($value) ? $value : []);
         }
     }
-
-    /**
-     * Get the category attribute.
-     */
-    public function getCategoryAttribute($value)
-    {
-        if (is_null($value)) {
-            return [];
-        }
-
-        if (is_string($value)) {
-            $decoded = json_decode($value, true);
-            return is_array($decoded) ? $decoded : [];
-        }
-
-        return is_array($value) ? $value : [];
-    }
-
-    /**
-     * Set the category attribute.
-     */
-    public function setCategoryAttribute($value)
-    {
-        if (is_string($value)) {
-            $this->attributes['category'] = json_encode([$value]);
-        } else {
-            $this->attributes['category'] = json_encode(is_array($value) ? $value : []);
-        }
-    }
 }
